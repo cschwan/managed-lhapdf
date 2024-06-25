e73928592e8e205e7e550c8bcf3e5a95a6aff47e
Christopher Schwan <handgranaten-herbert@posteo.de>
2024-06-25 12:04:32 +0200

    Remove superfluous build instructions

--- build.rs
+++ build.rs
@@ -16,20 +16,6 @@ fn main() {
         .flag_if_supported("-std=c++11")
         .compile("lhapdf-rust-cxx-bridge");
 
-    for lib_path in lhapdf.link_paths {
-        println!("cargo:rustc-link-search={}", lib_path.to_str().unwrap());
-    }
-
-    let link_modifier = if cfg!(feature = "static") {
-        "static="
-    } else {
-        ""
-    };
-
-    for lib in lhapdf.libs {
-        println!("cargo:rustc-link-lib={link_modifier}{lib}");
-    }
-
     println!("cargo:rerun-if-changed=include/wrappers.hpp");
     println!("cargo:rerun-if-changed=src/ffi.rs");
 }
